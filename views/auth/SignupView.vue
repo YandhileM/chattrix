35cff81b55497ddf129970ecb3a6ef067bee7983
Yandhile <yandhilemaluleke@gmail.com>
2025-06-03 15:11:09 +0200

    feat: add SignupView component for user registration functionality

---
+++ views/auth/SignupView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login-view">
+        <div class="login-container">
+            <!-- <h1>Login</h1> -->
+            <SignupForm />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import SignupForm from '@/components/auth/Signup.vue';
+</script>
+
+<style scoped>
+.login-view {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-color: var(--surface-ground);
+}
+
+.login-container {
+    max-width: 400px;
+    width: 100%;
+    padding: 2rem;
+}
+</style>
