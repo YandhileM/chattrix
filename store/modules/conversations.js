219828598f3dc48e1dd6f5d935f4cceaf08086ee
Yandhile <yandhilemaluleke@gmail.com>
2025-06-04 10:49:41 +0200

    style: format conversations and users store files for consistency

--- store/modules/conversations.js
+++ store/modules/conversations.js
@@ -4,12 +4,14 @@ export const useConversationsStore = defineStore('conversations', {
   state: () => ({
     // conversations state
   }),
-  
+
   getters: {
     // conversations getters
   },
-  
+
   actions: {
     // conversations actions
-  }
+  },
 })
+
+export default useConversationsStore
--- store/modules/users.js
+++ store/modules/users.js
@@ -4,12 +4,14 @@ export const useUsersStore = defineStore('users', {
   state: () => ({
     // users state
   }),
-  
+
   getters: {
     // users getters
   },
-  
+
   actions: {
     // users actions
-  }
+  },
 })
+
+export default useUsersStore
