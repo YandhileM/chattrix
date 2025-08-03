56132783a1dab184394b0ee30ed48b636c8ec823
Yandhile <yandhilemaluleke@gmail.com>
2025-08-03 14:13:25 +0200

    feat: add base styles, responsive design, and authentication components

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import { createApp } from 'vue'
-import PrimeVue from 'primevue/config'
+// import PrimeVue from 'primevue/config'
 import router from './router'
 import store from './store'
 import App from './App.vue'
 
 // Import global styles
 import './styles/main.css'
-import 'primeflex/primeflex.css'
+// import 'primeflex/primeflex.css'
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
-app.use(PrimeVue)
+// app.use(PrimeVue)
 
 app.mount('#app')
